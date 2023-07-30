0c1d1bcb70710ee39ab9b1c7158591d6012c0cc8
Tatsuya1117 <othe.world.pc@gmail.com>
2023-07-30 22:05:41 +0900

    update: new coments

--- resources/js/Pages/Inertia/Create.vue
+++ resources/js/Pages/Inertia/Create.vue
@@ -2,11 +2,12 @@
 <script setup>
   import { reactive } from "vue";
   import { Inertia } from "@inertiajs/inertia";
-
+  /** フォームに入力される値をリアクティブにする */
   const form = reactive({
     title: null,
     content: null,
   })
+  /** フォーム送信が実行された時に以下の関数を実行してサーバーに送信する */
   const submitFunction = () => {
     Inertia.post("/inertia", form)
   }
